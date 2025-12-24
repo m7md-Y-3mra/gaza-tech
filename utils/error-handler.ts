3310883e8153a3d0b0d01e48ca9ee6f01e52396b
m7md-Y-3mra <kingmoha2002@gmail.com>
2025-12-24 09:22:56 +0200

    feat: add error handler to wrapper every server action

---
+++ utils/error-handler.ts
@@ -0,0 +1,50 @@
+import z, { ZodError } from "zod";
+
+type ApiResponseSuccess<T> = {
+  success: true,
+  data: T,
+}
+
+type ApiResponseError = {
+  success: false,
+  errors: Record<string, string> | string
+}
+
+export function errorMiddleware<Args extends unknown[], Return>(
+  actionFn: (...args: Args) => Promise<Return>,
+) {
+  return async (...args: Args): Promise<ApiResponseSuccess<Return> | ApiResponseError> => {
+    try {
+      const data = await actionFn(...args);
+
+      return {
+        success: true,
+        data,
+      } as ApiResponseSuccess<Return>;
+    } catch (err: unknown) {
+      // ---------------------------
+      // Zod
+      // ---------------------------
+
+      if (err instanceof ZodError) {
+        const errors = z.flattenError(err, (issue) => issue.message).fieldErrors;
+
+        return {
+          success: false,
+          errors,
+        };
+      }
+
+      // ---------------------------
+      // Unknown error fallback
+      // ---------------------------
+
+      console.error("Unexpected Server Action Error:", err);
+
+      return {
+        success: false,
+        errors: "Unexpected Server Action Error"
+      };
+    }
+  };
+}
