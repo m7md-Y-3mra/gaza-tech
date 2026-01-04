5ef1b8351a3619af8c4080780bdf54bac1fabafb
m7md-Y-3mra <kingmoha2002@gmail.com>
2026-01-04 11:35:31 +0200

    feat: replace Geist font with Playpen Sans Arabic for improved typography

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,14 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import NextTopLoader from 'nextjs-toploader';
 import { Toaster } from 'sonner';
 import { ThemeProvider } from '@/providers/ThemeProvider';
+import { Playpen_Sans_Arabic } from 'next/font/google';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+const playpenArabic = Playpen_Sans_Arabic({
+  subsets: ['arabic', 'latin'],
+  weight: ['400', '500', '600', '700'],
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
@@ -27,9 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${playpenArabic.className} antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
