fe6d672a7e9f80959b5ede1ef023d54c8e71800c
m7md-Y-3mra <kingmoha2002@gmail.com>
2026-01-25 08:52:47 +0200

    refactor: separate the format price and is new in util function for reusability

---
+++ modules/listings/utils/format-price.ts
@@ -0,0 +1,5 @@
+export const formatPrice = (price: number, currency: string | null) => {
+    const currencySymbol = currency === 'USD' ? '$' : '₪';
+    const formattedPrice = `${currencySymbol}${price.toLocaleString()}`;
+    return formattedPrice;
+};
---
+++ modules/listings/utils/is-new.ts
@@ -0,0 +1,6 @@
+export const isNew = (productCondition: string) => {
+    const isNew = ['new', 'like new', 'brand new', 'good'].includes(
+        productCondition.toLowerCase()
+    );
+    return isNew;
+}
