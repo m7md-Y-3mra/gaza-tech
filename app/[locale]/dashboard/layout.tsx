fde27337792c12d3d1edbe3ffac116c1734df702
m7md-Y-3mra <kingmoha2002@gmail.com>
2026-02-23 14:32:24 +0200

    feat: add dashboard sidebar

---
+++ app/[locale]/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import DashboardSidebar from '@/modules/dashboard/components/dashboard-sidebar';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <DashboardSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+}
