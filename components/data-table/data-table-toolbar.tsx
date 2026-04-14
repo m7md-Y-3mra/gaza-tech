1c81ab90338b992f969ba822f6eb6f7b83a72a13
m7md-Y-3mra <kingmoha2002@gmail.com>
2026-04-14 14:53:06 +0300

    feat(data-table): implement data table components including column header, row actions, pagination, toolbar, and faceted filter; add command and popover UI components; update package dependencies

---
+++ components/data-table/data-table-toolbar.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import { X } from 'lucide-react';
+import { Table } from '@tanstack/react-table';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { DataTableViewOptions } from './data-table-view-options';
+
+import { DataTableFacetedFilter } from './data-table-faceted-filter';
+
+interface DataTableToolbarProps<TData> {
+  table: Table<TData>;
+  searchColumn?: string;
+  facetedFilters?: {
+    columnName: string;
+    title: string;
+    options: {
+      label: string;
+      value: string;
+      icon?: React.ComponentType<{ className?: string }>;
+    }[];
+  }[];
+}
+
+export function DataTableToolbar<TData>({
+  table,
+  searchColumn,
+  facetedFilters,
+}: DataTableToolbarProps<TData>) {
+  const isFiltered = table.getState().columnFilters.length > 0;
+
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex flex-1 items-center space-x-2">
+        {searchColumn && (
+          <Input
+            placeholder={`Filter ${searchColumn}...`}
+            value={
+              (table.getColumn(searchColumn)?.getFilterValue() as string) ?? ''
+            }
+            onChange={(event) =>
+              table.getColumn(searchColumn)?.setFilterValue(event.target.value)
+            }
+            className="h-8 w-[150px] lg:w-[250px]"
+          />
+        )}
+        {facetedFilters?.map(
+          (filter) =>
+            table.getColumn(filter.columnName) && (
+              <DataTableFacetedFilter
+                key={filter.columnName}
+                column={table.getColumn(filter.columnName)}
+                title={filter.title}
+                options={filter.options}
+              />
+            )
+        )}
+        {isFiltered && (
+          <Button
+            variant="ghost"
+            onClick={() => table.resetColumnFilters()}
+            className="h-8 px-2 lg:px-3"
+          >
+            Reset
+            <X className="ml-2 h-4 w-4" />
+          </Button>
+        )}
+      </div>
+      <DataTableViewOptions table={table} />
+    </div>
+  );
+}
